d17edf2f2bc67ea075baf881100cb48bff7d9542
Stefan Richter <stefan@02strich.de>
2012-01-17 11:43:59 +0100

    rewriting setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
 from setuptools import setup, find_packages
 
-version = '0.3'
-
-setup(name='pyrpm',
-      version=version,
-      description="A pure python rpm reader",
-      long_description=""" """,
-      classifiers=[],
-      keywords="",
-      author="Mario Morgado",
-      author_email="mjvm@caixamagica.pt",
-      url="",
-      license="BSD",
-      package_dir={'': '.'},
-      packages=find_packages(where='.')),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=['setuptools',
-                        ],
-      entry_points="""
-      # Add entry points here
-      """,
-      )
+setup(name='pyrpm-02strich',
+    version='0.3',
+    description="A pure python rpm reader and YUM metadata generator",
+    author="Stefan Richter",
+    author_email="stefan@02strich.de",
+    url="https://github.com/02strich/pyrpm",
+    license="BSD",
+    
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Topic :: Software Development :: Libraries',
+    ],
+    
+    packages=find_packages(where='.'),
+    use_2to3=True,
+)
