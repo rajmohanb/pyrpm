b1d762cde13b2df8f1d0fa0a308de14cbd527930
Stefan Richter <stefan@02strich.de>
2012-01-28 22:59:15 +0100

    fix etree-text issue and one missing value

--- pyrpm/yum.py
+++ pyrpm/yum.py
@@ -2,31 +2,38 @@ from xml.etree.ElementTree import Element
 
 from rpm import RPM
 
+# shorthand function for Element creation
+#################
+def element(tag, attrib={}, text=None):
+    ele = Element(tag, attrib)
+    if text: ele.text = text
+    return ele
+
 ##################
 # Real classes
 #################
 class YumPackage(RPM):  
-    def _xml_base_items(self, element):
-        element.append(Element('{http://linux.duke.edu/metadata/common}name', text=self.header.name))
-        element.append(Element('{http://linux.duke.edu/metadata/common}arch', text=self.header.architecture))
-        element.append(Element("{http://linux.duke.edu/metadata/common}version", {'epoch': str(self.header.epoch), 'ver': self.header.version, 'rel': self.header.release}))
-        element.append(Element('{http://linux.duke.edu/metadata/common}checksum', {'type': 'sha256', 'pkgid': 'YES'}, text=self.checksum))
-        element.append(Element('{http://linux.duke.edu/metadata/common}summary', text=self.header.summary))
-        element.append(Element('{http://linux.duke.edu/metadata/common}description', text=self.header.description))
-        element.append(Element('{http://linux.duke.edu/metadata/common}packager', text=self.header.packager))
-        element.append(Element('{http://linux.duke.edu/metadata/common}url', text=self.header.url))
-        element.append(Element('{http://linux.duke.edu/metadata/common}time', {'file': '0', 'build': str(self.header.build_time)}))
-        element.append(Element('{http://linux.duke.edu/metadata/common}size', {'package': str(self.header.size), 'installed': str(sum([file.size for file in self.filelist])), 'archive': str(self.header.archive_size)}))
-        element.append(Element('{http://linux.duke.edu/metadata/common}location', {'href':self.canonical_filename}))
+    def _xml_base_items(self, ele):
+        ele.append(element('{http://linux.duke.edu/metadata/common}name', text=self.header.name))
+        ele.append(element('{http://linux.duke.edu/metadata/common}arch', text=self.header.architecture))
+        ele.append(element("{http://linux.duke.edu/metadata/common}version", {'epoch': str(self.header.epoch), 'ver': self.header.version, 'rel': self.header.release}))
+        ele.append(element('{http://linux.duke.edu/metadata/common}checksum', {'type': 'sha256', 'pkgid': 'YES'}, text=self.checksum))
+        ele.append(element('{http://linux.duke.edu/metadata/common}summary', text=self.header.summary))
+        ele.append(element('{http://linux.duke.edu/metadata/common}description', text=self.header.description))
+        ele.append(element('{http://linux.duke.edu/metadata/common}packager', text=self.header.packager))
+        ele.append(element('{http://linux.duke.edu/metadata/common}url', text=self.header.url))
+        ele.append(element('{http://linux.duke.edu/metadata/common}time', {'file': str(self.header.build_time), 'build': str(self.header.build_time)}))
+        ele.append(element('{http://linux.duke.edu/metadata/common}size', {'package': str(self.header.size), 'installed': str(sum([file.size for file in self.filelist])), 'archive': str(self.header.archive_size)}))
+        ele.append(element('{http://linux.duke.edu/metadata/common}location', {'href':self.canonical_filename}))
     
 
-    def _xml_format_items(self, element):
-        ef = Element('{http://linux.duke.edu/metadata/common}format')
-        ef.append(Element('{http://linux.duke.edu/metadata/rpm}license', text=self.header.license))
-        ef.append(Element('{http://linux.duke.edu/metadata/rpm}vendor', text=self.header.vendor))
-        ef.append(Element('{http://linux.duke.edu/metadata/rpm}group', text=self.header.group))
-        ef.append(Element('{http://linux.duke.edu/metadata/rpm}buildhost', text=self.header.build_host))
-        ef.append(Element('{http://linux.duke.edu/metadata/rpm}sourcerpm', text=self.header.source_rpm))
+    def _xml_format_items(self, ele):
+        ef = element('{http://linux.duke.edu/metadata/common}format')
+        ef.append(element('{http://linux.duke.edu/metadata/rpm}license', text=self.header.license))
+        ef.append(element('{http://linux.duke.edu/metadata/rpm}vendor', text=self.header.vendor))
+        ef.append(element('{http://linux.duke.edu/metadata/rpm}group', text=self.header.group))
+        ef.append(element('{http://linux.duke.edu/metadata/rpm}buildhost', text=self.header.build_host))
+        ef.append(element('{http://linux.duke.edu/metadata/rpm}sourcerpm', text=self.header.source_rpm))
         
         self._xml_pco(ef, 'provides')
         self._xml_requires(ef)
@@ -34,18 +41,18 @@ class YumPackage(RPM):
         self._xml_pco(ef, 'obsoletes')         
         self._xml_files(ef, True)
         
-        element.append(ef)
+        ele.append(ef)
 
 
-    def _xml_pco(self, element, pcotype):
+    def _xml_pco(self, ele, pcotype):
         # get right pco
         mylist = getattr(self, pcotype)
         if not mylist:
             return
         
-        ef = Element('{http://linux.duke.edu/metadata/rpm}' + pcotype)
+        ef = element('{http://linux.duke.edu/metadata/rpm}' + pcotype)
         for prco in sorted(mylist):
-            entry = Element('{http://linux.duke.edu/metadata/rpm}entry', {'name': prco.name})
+            entry = element('{http://linux.duke.edu/metadata/rpm}entry', {'name': prco.name})
             if prco.str_flags:
                 entry.set('flags', prco.str_flags)
                 e,v,r = prco.version
@@ -56,10 +63,10 @@ class YumPackage(RPM):
                 if r:
                     entry.set('rel', r)
             ef.append(entry)
-        element.append(ef)
+        ele.append(ef)
     
     
-    def _xml_files(self, element, primary=False):
+    def _xml_files(self, ele, primary=False):
         # sort files
         files = {'file': [], 'dir': [], 'ghost': []}
         for file in self.filelist:
@@ -69,16 +76,16 @@ class YumPackage(RPM):
         
         # create output
         for file in sorted(files['file']):
-            element.append(Element("{http://linux.duke.edu/metadata/filelists}file", text=file.name))
+            ele.append(element("{http://linux.duke.edu/metadata/filelists}file", text=file.name))
         
         for type in ['dir', 'ghost']:
             for file in sorted(files[type]):
-                element.append(Element("{http://linux.duke.edu/metadata/filelists}file", {'type': type}, text=file.name))
+                ele.append(element("{http://linux.duke.edu/metadata/filelists}file", {'type': type}, text=file.name))
     
 
-    def _xml_requires(self, element):
+    def _xml_requires(self, ele):
         """returns deps in XML format"""
-        ef = Element('{http://linux.duke.edu/metadata/rpm}requires')
+        ef = element('{http://linux.duke.edu/metadata/rpm}requires')
         used = 0
         
         for prco in sorted(self.requires):
@@ -93,7 +100,7 @@ class YumPackage(RPM):
                     if prco in self.provides:
                         continue
             
-            entry = Element('{http://linux.duke.edu/metadata/rpm}entry', {'name': prco.name})
+            entry = element('{http://linux.duke.edu/metadata/rpm}entry', {'name': prco.name})
             if prco.str_flags:
                 entry.set('flags', prco.str_flags)
                 e,v,r = prco.version
@@ -110,35 +117,35 @@ class YumPackage(RPM):
             used += 1
             
         if used != 0:
-            element.append(ef)
+            ele.append(ef)
 
 
-    def _xml_changelog(self, element, clog_limit=0):
+    def _xml_changelog(self, ele, clog_limit=0):
         if not self.changelog:
             return ""
         
         # We need to output them "backwards", so the oldest is first
         clogs = self.changelog[:clog_limit] if clog_limit else self.changelog
         for changelog in clogs:
-            element.append(Element('{http://linux.duke.edu/metadata/other}changelog', {'author': changelog.name, 'date': str(changelog.time)}, text=changelog.text))
+            ele.append(element('{http://linux.duke.edu/metadata/other}changelog', {'author': changelog.name, 'date': str(changelog.time)}, text=changelog.text))
 
 
     def xml_primary_metadata(self):
-        e = Element("{http://linux.duke.edu/metadata/common}package", {'type': 'rpm'})
-        self._xml_base_items(e)
-        self._xml_format_items(e)
-        return e
+        ele = element("{http://linux.duke.edu/metadata/common}package", {'type': 'rpm'})
+        self._xml_base_items(ele)
+        self._xml_format_items(ele)
+        return ele
     
     
     def xml_filelists_metadata(self):
-        e = Element("{http://linux.duke.edu/metadata/filelists}package", {'pkgid': self.checksum, 'name': self.header.name, 'arch': self.header.architecture})
-        e.append(Element("{http://linux.duke.edu/metadata/filelists}version", {'epoch': str(self.header.epoch), 'ver': self.header.version, 'rel': self.header.release}))
-        self._xml_files(e)
-        return e
+        ele = element("{http://linux.duke.edu/metadata/filelists}package", {'pkgid': self.checksum, 'name': self.header.name, 'arch': self.header.architecture})
+        ele.append(element("{http://linux.duke.edu/metadata/filelists}version", {'epoch': str(self.header.epoch), 'ver': self.header.version, 'rel': self.header.release}))
+        self._xml_files(ele)
+        return ele
     
     
     def xml_other_metadata(self, clog_limit=0):
-        e = Element("{http://linux.duke.edu/metadata/other}package", {'pkgid': self.checksum, 'name': self.header.name, 'arch': self.header.architecture})
-        e.append(Element("{http://linux.duke.edu/metadata/other}version", {'epoch': str(self.header.epoch), 'ver': self.header.version, 'rel': self.header.release}))
-        self._xml_changelog(e, clog_limit)
-        return e
+        ele = element("{http://linux.duke.edu/metadata/other}package", {'pkgid': self.checksum, 'name': self.header.name, 'arch': self.header.architecture})
+        ele.append(element("{http://linux.duke.edu/metadata/other}version", {'epoch': str(self.header.epoch), 'ver': self.header.version, 'rel': self.header.release}))
+        self._xml_changelog(ele, clog_limit)
+        return ele
