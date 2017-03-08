bac412ae0de3cbb6cc037cb96ce489d866600876
Rajmohan Banavi <rbanavi@shoretel.com>
2017-03-08 15:47:22 +0530

    Adding proper location href attribute in the primary xml
    Adding a canonical name does not add the absolute path of the rpm file
    to the location attribute. This results in the packages not being
    available when queries using yum client.

--- pyrpm/yum.py
+++ pyrpm/yum.py
@@ -26,7 +26,7 @@ class YumPackage(RPM):
         ele.append(element('{http://linux.duke.edu/metadata/common}url', text=self.header.url))
         ele.append(element('{http://linux.duke.edu/metadata/common}time', {'file': str(self.header.build_time), 'build': str(self.header.build_time)}))
         ele.append(element('{http://linux.duke.edu/metadata/common}size', {'package': str(self.filesize), 'installed': str(sum([file.size for file in self.filelist])), 'archive': str(self.header.archive_size)}))
-        ele.append(element('{http://linux.duke.edu/metadata/common}location', {'href': self.canonical_filename}))
+        ele.append(element('{http://linux.duke.edu/metadata/common}location', {'href': self.rpmfile.name}))
 
     def _xml_format_items(self, ele):
         ef = element('{http://linux.duke.edu/metadata/common}format')
